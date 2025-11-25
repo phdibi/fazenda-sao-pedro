7ed6e81043e644ce4234a6c019a64e8db4254968
phdibi <phdibi@gmail.com>
2025-11-25 07:51:51 -0300

    Update vite.config.ts

--- fazenda-sao-pedro/vite.config.ts
+++ fazenda-sao-pedro/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+        build: {
+        outDir: 'dist',  // ← ADICIONA ESTA LINHA
+    },
     server: {
         port: 3000,
         host: '0.0.0.0',
