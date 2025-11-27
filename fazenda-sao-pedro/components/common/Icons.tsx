0f1e8ebb6e42e1268b96512fffc5583747939a07
phdibi <phdibi@gmail.com>
2025-11-27 15:21:29 -0300

    Update Icons.tsx

--- fazenda-sao-pedro/components/common/Icons.tsx
+++ fazenda-sao-pedro/components/common/Icons.tsx
@@ -352,3 +352,8 @@ export const AdjustmentsHorizontalIcon = ({ className }: IconProps) => (
     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
   </svg>
 );
+export const ArrowsRightLeftIcon = ({ className }: IconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className || 'w-6 h-6'}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+  </svg>
+);
